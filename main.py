9e4aab3c9ecf145eff7869c480771e62a462295d
ElecarnoYT <benrobmc.mc@gmail.com>
2024-06-11 21:24:38 +0100

    aether & avoidance force

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ COL_GREEN = (0, 200, 0)
 COL_BLUE =  (0, 0, 200)
 COL_YELLOW = (200, 200, 0)
 
+FONT = pygame.font.SysFont("couriernew", 16)
+
 class Particle:    
     TIMESTEP = 1 / 60
     RADIUS = 5
@@ -33,6 +35,8 @@ class Particle:
         x = self.x + WIDTH / 2
         y = self.y + HEIGHT / 2
 
+        pygame.draw.circle(win, self.color, (x, y), self.RADIUS)
+
         if len(self.path) > 2:
             updated_points = []
             for point in self.path:
@@ -47,40 +51,13 @@ class Particle:
             if show_trails:
                 pygame.draw.lines(win, self.color, False, updated_points, 2)
 
-        vector_scale = 0.05
+        vector_scale = 0.2
         if show_vectors:
             pygame.draw.line(win, COL_RED, (x,y), (x, y+self.y_vel*vector_scale), 2)
             pygame.draw.line(win, COL_GREEN, (x,y), (x+self.x_vel*vector_scale, y), 2)
             pygame.draw.line(win, COL_YELLOW, (x,y), (x+self.x_vel*vector_scale, y+self.y_vel*vector_scale), 2)
 
-        pygame.draw.circle(win, self.color, (x, y), self.RADIUS)
-
-    def attraction(self, other):
-        other_x, other_y = other.x, other.y
-        distance_x = other_x - self.x
-        distance_y = other_y - self.y
-        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
-
-        force = distance
-        theta = math.atan2(distance_y, distance_x)
-        force_x = math.cos(theta) * force
-        force_y = math.sin(theta) * force
-
-        return force_x, force_y
-    
-    def update_position(self, particles):
-        total_fx = total_fy = 0
-        for particle in particles:
-            if self == particle:
-                continue
-
-            fx, fy = self.attraction(particle)
-            total_fx = fx
-            total_fy = fy
-
-        self.x_vel += total_fx * self.TIMESTEP
-        self.y_vel += total_fy * self.TIMESTEP
-
+    def update_position(self):
         self.x += self.x_vel * self.TIMESTEP
         self.y += self.y_vel * self.TIMESTEP
 
@@ -98,6 +75,46 @@ class Particle:
         self.path.append((self.x, self.y))
 
 
+class Aether(Particle):
+    def __init__(self, x, y, color, size):
+        super().__init__(x, y, color)
+        self.size = size
+
+    def draw_size(self, win):
+        x = self.x + WIDTH / 2
+        y = self.y + HEIGHT / 2
+
+        size_text = FONT.render(f"{round(self.size, 1)}", 1, COL_WHITE)
+        win.blit(size_text, (x - size_text.get_width()/2, y+10))
+
+    def avoidance(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        force = -(1 / (self.size * math.sqrt(distance))) * 100
+
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+
+        return force_x, force_y
+
+    def calculate_velocities(self, particles):
+        total_fx = total_fy = 0
+        for particle in particles:
+            if self == particle:
+                continue
+
+            fx, fy = self.avoidance(particle)
+            total_fx = fx
+            total_fy = fy
+
+        self.x_vel += total_fx * self.TIMESTEP
+        self.y_vel += total_fy * self.TIMESTEP
+
+
 def main():
     global show_vectors
     global show_trails
@@ -107,13 +124,15 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    particles = []
+    aether_particles = []
 
-    for i in range(100):
-        x = random.randrange(-WIDTH/2, WIDTH/2)
-        y = random.randrange(-HEIGHT/2, HEIGHT/2)
-        particle = Particle(x, y, COL_WHITE)
-        particles.append(particle)
+    for i in range(20):
+        fac = 2
+        x = random.randrange(-WIDTH/fac, WIDTH/fac)
+        y = random.randrange(-HEIGHT/fac, HEIGHT/fac)
+        size = random.uniform(0.1, 1)
+        aether = Aether(x, y, COL_WHITE, size)
+        aether_particles.append(aether)
 
     while run:
         clock.tick(60)
@@ -129,9 +148,11 @@ def main():
                     show_trails = not show_trails
 
 
-        for particle in particles:
-            particle.update_position(particles)
-            particle.draw(WIN)
+        for aether in aether_particles:
+            aether.calculate_velocities(aether_particles)
+            aether.update_position()
+            aether.draw(WIN)
+            aether.draw_size(WIN)
         
         pygame.display.update()
 
